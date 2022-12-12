d5d5c6be9230903f0e61a08e75328e1acf3c61cf
gnasnik <frankffenn@gmail.com>
2022-12-12 19:16:04 +0800

    chore: Update router from api/v0 to api/v1

--- api/router.go
+++ api/router.go
@@ -9,7 +9,7 @@ import (
 var log = logging.Logger("api")
 
 func ConfigRouter(router *gin.Engine, cfg config.Config) {
-	apiV0 := router.Group("/api/v0")
+	apiV1 := router.Group("/api/v1")
 	authMiddleware, err := jwtGinMiddleware(cfg.SecretKey)
 	if err != nil {
 		log.Fatalf("jwt auth middleware: %v", err)
@@ -20,33 +20,33 @@ func ConfigRouter(router *gin.Engine, cfg config.Config) {
 		log.Fatalf("authMiddleware.MiddlewareInit: %v", err)
 	}
 
-	apiV0.POST("/login", authMiddleware.LoginHandler)
-	apiV0.POST("/logout", authMiddleware.LogoutHandler)
-	apiV0.GET("/refresh_token", authMiddleware.RefreshHandler)
+	apiV1.POST("/login", authMiddleware.LoginHandler)
+	apiV1.POST("/logout", authMiddleware.LogoutHandler)
+	apiV1.GET("/refresh_token", authMiddleware.RefreshHandler)
 
 	// dashboard
-	apiV0.GET("/all_areas", GetAllAreas)
-	apiV0.GET("/schedulers", GetSchedulersHandler)
-	apiV0.GET("/get_user_device_info", GetUserDeviceInfoHandler)
-	apiV0.GET("/get_index_info", GetIndexInfoHandler)
-	apiV0.GET("/get_device_info", GetDeviceInfoHandler)
-	apiV0.GET("/get_device_info_daily", GetDeviceInfoDailyHandler)
-	apiV0.GET("/get_diagnosis_days", GetDeviceDiagnosisDailyHandler)
-	apiV0.GET("/get_diagnosis_hours", GetDeviceDiagnosisHourHandler)
-	apiV0.GET("/get_cache_list", GetCacheListHandler)
-	apiV0.GET("/get_retrieve_list", GetRetrieveListHandler)
-	apiV0.POST("/create_application", CreateApplicationHandler)
-	apiV0.GET("/get_applications", GetApplicationsHandler)
+	apiV1.GET("/all_areas", GetAllAreas)
+	apiV1.GET("/schedulers", GetSchedulersHandler)
+	apiV1.GET("/get_user_device_info", GetUserDeviceInfoHandler)
+	apiV1.GET("/get_index_info", GetIndexInfoHandler)
+	apiV1.GET("/get_device_info", GetDeviceInfoHandler)
+	apiV1.GET("/get_device_info_daily", GetDeviceInfoDailyHandler)
+	apiV1.GET("/get_diagnosis_days", GetDeviceDiagnosisDailyHandler)
+	apiV1.GET("/get_diagnosis_hours", GetDeviceDiagnosisHourHandler)
+	apiV1.GET("/get_cache_list", GetCacheListHandler)
+	apiV1.GET("/get_retrieve_list", GetRetrieveListHandler)
+	apiV1.POST("/create_application", CreateApplicationHandler)
+	apiV1.GET("/get_applications", GetApplicationsHandler)
 
 	// console
-	apiV0.GET("/device_binding", DeviceBindingHandler)
-	apiV0.GET("/create_task", CreateTaskHandler)
-	apiV0.GET("/get_task", GetTaskInfoHandler)
-	apiV0.GET("/get_task_list", GetTaskListHandler)
-	apiV0.GET("/get_task_detail", GetTaskDetailHandler)
+	apiV1.GET("/device_binding", DeviceBindingHandler)
+	apiV1.GET("/create_task", CreateTaskHandler)
+	apiV1.GET("/get_task", GetTaskInfoHandler)
+	apiV1.GET("/get_task_list", GetTaskListHandler)
+	apiV1.GET("/get_task_detail", GetTaskDetailHandler)
 
 	// admin
-	admin := apiV0.Group("/admin")
+	admin := apiV1.Group("/admin")
 	admin.Use(authMiddleware.MiddlewareFunc())
 	admin.GET("/cache_task_list", GetCacheTaskListHandler)
 	admin.GET("/cache_task_info", GetCacheTaskInfoHandler)
