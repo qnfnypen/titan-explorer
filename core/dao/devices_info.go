3b36d0a1a786e8ec4b8efd52f46ced84592e9751
gnasnik <frankffenn@gmail.com>
2022-12-15 14:37:09 +0800

    fix: candidate count

--- core/dao/devices_info.go
+++ core/dao/devices_info.go
@@ -154,7 +154,7 @@ func upsertDeviceInfoStatement() string {
 
 func CountFullNodeInfo(ctx context.Context) (*model.FullNodeInfo, error) {
 	queryStatement := fmt.Sprintf(`SELECT count( device_id ) AS total_node_count ,  SUM(IF(node_type = 1, 1, 0)) AS edge_count, 
-       SUM(IF(node_type = 2 OR node_type = 3, 1, 0)) AS candidate_count, SUM(IF(node_type = 3, 1, 0)) AS validator_count, SUM( disk_space) AS total_storage, 
+       SUM(IF(node_type = 2, 1, 0)) AS candidate_count, SUM(IF(node_type = 3, 1, 0)) AS validator_count, SUM( disk_space) AS total_storage, 
        SUM(bandwidth_up) AS total_upstream_bandwidth, SUM(bandwidth_down) AS total_downstream_bandwidth FROM %s;`, tableNameDeviceInfo)
 
 	var out model.FullNodeInfo
