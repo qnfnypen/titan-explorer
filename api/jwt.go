daf4b02ffeda9060ac4f8ee957a52cb444e40479
gnasnik <frankffenn@gmail.com>
2024-03-01 15:41:37 +0800

    fix login params

--- api/jwt.go
+++ api/jwt.go
@@ -73,17 +73,22 @@ func jwtGinMiddleware(secretKey string) (*jwt.GinJWTMiddleware, error) {
 			})
 		},
 		Authenticator: func(c *gin.Context) (interface{}, error) {
-			var loginParams login
-			if err := c.BindJSON(&loginParams); err != nil {
-				log.Errorf("login bindjson: %v", err)
-			}
-
-			if loginParams == (login{}) {
-				loginParams = login{
-					Username:   c.Query("username"),
-					VerifyCode: c.Query("verify_code"),
-					Password:   c.Query("password"),
-				}
+			//var loginParams login
+			//if err := c.BindJSON(&loginParams); err != nil {
+			//	log.Errorf("login bindjson: %v", err)
+			//}
+			//
+			//if loginParams == (login{}) {
+			//	loginParams = login{
+			//		Username:   c.Query("username"),
+			//		VerifyCode: c.Query("verify_code"),
+			//		Password:   c.Query("password"),
+			//	}
+			//}
+			loginParams := login{
+				Username:   c.Query("username"),
+				VerifyCode: c.Query("verify_code"),
+				Password:   c.Query("password"),
 			}
 
 			signature := c.Query("sign")
