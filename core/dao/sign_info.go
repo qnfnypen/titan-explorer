a34ea17fcfe54f580dff3727407d5155a16732c0
nezha <448688985@qq.com>
2024-03-01 10:39:28 +0800

    add sql interface

--- core/dao/sign_info.go
+++ core/dao/sign_info.go
@@ -4,8 +4,15 @@ import (
 	"github.com/gnasnik/titan-explorer/core/generated/model"
 )
 
-func InsertSignInfo(info *model.SignInfo) error {
-	_, err := DB.NamedExec("INSERT INTO sign_info (miner_id, address, date, signed_msg) VALUES (:miner_id, :address, :date, :signed_msg)", info)
+func GetInfoByMinerID(minerID string) (model.SignInfo, error) {
+	var info model.SignInfo
+	err := DB.Get(&info, "SELECT * FROM sign_info WHERE miner_id = ?", minerID)
+
+	return info, err
+}
+
+func ReplaceSignInfo(info *model.SignInfo) error {
+	_, err := DB.NamedExec("REPLACE INTO sign_info (miner_id, address, date, signed_msg) VALUES (:miner_id, :address, :date, :signed_msg)", info)
 
 	return err
 }
