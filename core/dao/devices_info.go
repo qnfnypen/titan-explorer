1ff91515fe2690ed29ee7b365eb001dc4e06f910
gnasnik <frankffenn@gmail.com>
2024-03-14 15:50:07 +0800

    update device rank by cumulative_profit

--- core/dao/devices_info.go
+++ core/dao/devices_info.go
@@ -478,7 +478,7 @@ func RankDeviceInfo(ctx context.Context) error {
 	tx := DB.MustBegin()
 	defer tx.Rollback()
 	tx.MustExec("SET @r=0;")
-	queryStatement := fmt.Sprintf(`UPDATE %s SET device_rank= @r:= (@r+1) ORDER BY device_status DESC, node_type DESC;`, tableNameDeviceInfo)
+	queryStatement := fmt.Sprintf(`UPDATE %s SET device_rank= @r:= (@r+1) ORDER BY device_status DESC, node_type DESC, cumulative_profit DESC;`, tableNameDeviceInfo)
 	_, err := tx.ExecContext(ctx, queryStatement)
 	if err != nil {
 		return err
