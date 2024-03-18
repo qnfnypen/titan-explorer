f9b7c8c376c977032b581974e0a82246cc5609ea
gnasnik <frankffenn@gmail.com>
2024-03-18 18:08:58 +0800

    Update device profit sql

--- core/dao/devices_info.go
+++ core/dao/devices_info.go
@@ -488,17 +488,35 @@ func BulkInsertOrUpdateDeviceStatus(ctx context.Context, deviceInfos []*model.De
 }
 
 func BulkUpdateDeviceInfo(ctx context.Context, deviceInfos []*model.DeviceInfo) error {
-	for _, device := range deviceInfos {
-		_, err := DB.NamedExecContext(ctx, fmt.Sprintf(
-			`UPDATE %s SET today_online_time = :today_online_time, today_profit = :today_profit,
-				yesterday_profit = :yesterday_profit, seven_days_profit = :seven_days_profit, month_profit = :month_profit, 
-				updated_at = now() WHERE device_id = :device_id`, tableNameDeviceInfo),
-			device)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	insertStatement := fmt.Sprintf(
+		`INSERT INTO %s (
+                	device_id, node_type, device_name, user_id, system_version,  active_status,network_info, external_ip, internal_ip, ip_location,
+                	ip_country, ip_province, ip_city, latitude, longitude, mac_location, cpu_usage, memory_usage, cpu_cores, memory, disk_usage, disk_space,
+                	device_status, device_status_code, io_system, online_time, today_online_time, today_profit, yesterday_profit, seven_days_profit, month_profit, area_id,
+                	cumulative_profit, bandwidth_up, bandwidth_down,download_traffic,upload_traffic, created_at, updated_at, bound_at,cache_count,retrieval_count, nat_type, income_incr
+                	)
+				VALUES (
+					:device_id, :node_type, :device_name, :user_id,  :system_version, :active_status,:network_info, :external_ip, :internal_ip, :ip_location,
+					:ip_country, :ip_province, :ip_city, :latitude, :longitude, :mac_location,:cpu_usage, :memory_usage, :cpu_cores, :memory, :disk_usage, :disk_space,
+					:device_status, :device_status_code, :io_system, :online_time, :today_online_time, :today_profit,:yesterday_profit, :seven_days_profit, :month_profit, :area_id,
+					:cumulative_profit, :bandwidth_up, :bandwidth_down,:download_traffic,:upload_traffic, now(), now(),:bound_at,:cache_count,:retrieval_count, :nat_type, :income_incr
+				)`, tableNameDeviceInfo,
+	)
+	updateStatement := ` ON DUPLICATE KEY UPDATE today_online_time = VALUES(today_online_time), today_profit = VALUES(today_profit), yesterday_profit = VALUES(yesterday_profit),seven_days_profit = VALUES(seven_days_profit),
+month_profit = VALUES(month_profit),today_online_time = VALUES(today_online_time), updated_at = now()`
+	_, err := DB.NamedExecContext(ctx, insertStatement+updateStatement, deviceInfos)
+	return err
+	//for _, device := range deviceInfos {
+	//	_, err := DB.NamedExecContext(ctx, fmt.Sprintf(
+	//		`UPDATE %s SET today_online_time = :today_online_time, today_profit = :today_profit,
+	//			yesterday_profit = :yesterday_profit, seven_days_profit = :seven_days_profit, month_profit = :month_profit,
+	//			updated_at = now() WHERE device_id = :device_id`, tableNameDeviceInfo),
+	//		device)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//return nil
 }
 
 func upsertDeviceInfoStatement() string {
