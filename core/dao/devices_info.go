ee17529ef800db7fced97d1aba929d6a11068706
gnasnik <frankffenn@gmail.com>
2024-04-01 15:13:15 +0800

    return all online external IPs to map API

--- core/dao/devices_info.go
+++ core/dao/devices_info.go
@@ -78,7 +78,8 @@ func GetDeviceMapInfo(ctx context.Context, lang model.Language, deviceId string)
     SUBSTRING_INDEX(t.external_ip, '.', 1), 
     '.xxx.xxx.', 
     SUBSTRING_INDEX(t.external_ip, '.', -1)
-  ) AS ip, t.node_type, t.longitude, t.latitude from  (select IF(lc.city <> '', lc.city, lc.country) as name, external_ip , d.node_type, d.longitude, d.latitude from device_info d  left join %s lc on d.external_ip = lc.ip  where device_status_code = 1 %s) t  group by t.external_ip`, location, where)
+  ) AS ip, t.node_type, t.longitude, t.latitude from  (select IF(lc.city <> '', lc.city, lc.country) as name, external_ip , d.node_type, d.longitude, d.latitude from device_info d  
+      left join %s lc on d.external_ip = lc.ip  where device_status_code = 1 %s) t`, location, where)
 
 	rows, err := DB.QueryxContext(ctx, query)
 	if err != nil {
