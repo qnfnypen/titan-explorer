d79fa26d671fad48b832e0899df586d09e2e2b0a
gnasnik <frankffenn@gmail.com>
2024-04-09 19:16:17 +0800

    fix missing update device income_incr

--- core/dao/devices_info.go
+++ core/dao/devices_info.go
@@ -437,13 +437,13 @@ func BulkAddDeviceInfo(ctx context.Context, deviceInfos []*model.DeviceInfo) err
                 	device_id, node_type, device_name, user_id, system_version,  active_status,network_info, external_ip, internal_ip, ip_location, device_rank,
                 	ip_country, ip_province, ip_city, latitude, longitude, mac_location, cpu_usage, memory_usage, cpu_cores, memory, disk_usage, disk_space,
                 	device_status, device_status_code, io_system, online_time, today_online_time, today_profit, yesterday_profit, seven_days_profit, month_profit,
-                	cumulative_profit, bandwidth_up, bandwidth_down,download_traffic,upload_traffic, created_at, updated_at, bound_at,cache_count,retrieval_count, area_id
+                	cumulative_profit, bandwidth_up, bandwidth_down,download_traffic,upload_traffic, created_at, updated_at, bound_at,cache_count,retrieval_count, area_id, income_incr
                 	)
 				VALUES (
 					:device_id, :node_type, :device_name, :user_id,  :system_version, :active_status,:network_info, :external_ip, :internal_ip, :ip_location, :device_rank,
 					:ip_country, :ip_province, :ip_city, :latitude, :longitude, :mac_location,:cpu_usage, :memory_usage, :cpu_cores, :memory, :disk_usage, :disk_space,
 					:device_status, :device_status_code, :io_system, :online_time, :today_online_time, :today_profit,:yesterday_profit, :seven_days_profit, :month_profit,
-					:cumulative_profit, :bandwidth_up, :bandwidth_down,:download_traffic,:upload_traffic, now(), now(),:bound_at,:cache_count,:retrieval_count, :area_id
+					:cumulative_profit, :bandwidth_up, :bandwidth_down,:download_traffic,:upload_traffic, now(), now(),:bound_at,:cache_count,:retrieval_count, :area_id, :income_incr
 				)`, tableNameDeviceInfo,
 	)
 	_, err := DB.NamedExecContext(ctx, statement, deviceInfos)
