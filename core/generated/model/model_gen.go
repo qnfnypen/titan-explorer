2de7aebcb578617a56c3eab9094c9e451b329334
gnasnik <frankffenn@gmail.com>
2024-06-28 12:55:35 +0800

    revert user filed

--- core/generated/model/model_gen.go
+++ core/generated/model/model_gen.go
@@ -304,32 +304,33 @@ type SystemInfo struct {
 }
 
 type User struct {
-	ID                       int64     `db:"id" json:"id"`
-	Uuid                     string    `db:"uuid" json:"uuid"`
-	Avatar                   string    `db:"avatar" json:"avatar"`
-	Username                 string    `db:"username" json:"username"`
-	PassHash                 string    `db:"pass_hash" json:"-"`
-	UserEmail                string    `db:"user_email" json:"user_email"`
-	WalletAddress            string    `db:"wallet_address" json:"wallet_address"`
-	Role                     int32     `db:"role" json:"role"`
-	AllocateStorage          int       `db:"allocate_storage" json:"allocate_storage"`
-	ProjectId                int64     `db:"project_id"`
-	Referrer                 string    `db:"referrer" json:"referrer"`
-	ReferrerUserId           string    `db:"referrer_user_id" json:"-"`
-	ReferralCode             string    `db:"referral_code" json:"referral_code"`
-	Reward           		 float64 `db:"reward" json:"reward"`
-	ReferralReward   		 float64 `db:"referral_reward" json:"referral_reward"`
-	ClosedTestReward 		 float64 `db:"closed_test_reward" json:"closed_test_reward"`
-	HuygensReward            float64   `db:"huygens_reward" json:"huygens_reward"`
-	HuygensReferralReward    float64   `db:"huygens_referral_reward" json:"huygens_referral_reward"`
-	HerschelReward           float64   `db:"herschel_reward" json:"herschel_reward"`
-	HerschelReferralReward   float64   `db:"herschel_referral_reward" json:"herschel_referral_reward"`
-	CassiniReward  			 float64   `db:"cassini_reward" json:"cassini_reward"`
-	CassiniReferralReward    float64   `db:"cassini_referral_reward" json:"cassini_referral_reward"`
-	DeviceCount              int64     `db:"device_count" json:"device_count"`
-	CreatedAt                time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt                time.Time `db:"updated_at" json:"-"`
-	DeletedAt                time.Time `db:"deleted_at" json:"-"`
+	ID                     int64     `db:"id" json:"id"`
+	Uuid                   string    `db:"uuid" json:"uuid"`
+	Avatar                 string    `db:"avatar" json:"avatar"`
+	Username               string    `db:"username" json:"username"`
+	PassHash               string    `db:"pass_hash" json:"-"`
+	UserEmail              string    `db:"user_email" json:"user_email"`
+	WalletAddress          string    `db:"wallet_address" json:"wallet_address"`
+	Role                   int32     `db:"role" json:"role"`
+	AllocateStorage        int       `db:"allocate_storage" json:"allocate_storage"`
+	ProjectId              int64     `db:"project_id"`
+	Referrer               string    `db:"referrer" json:"referrer"`
+	ReferrerUserId         string    `db:"referrer_user_id" json:"-"`
+	ReferralCode           string    `db:"referral_code" json:"referral_code"`
+	Reward                 float64   `db:"reward" json:"reward"`
+	ReferralReward         float64   `db:"referral_reward" json:"referral_reward"`
+	ClosedTestReward       float64   `db:"closed_test_reward" json:"closed_test_reward"`
+	HuygensReward          float64   `db:"huygens_reward" json:"huygens_reward"`
+	HuygensReferralReward  float64   `db:"huygens_referral_reward" json:"huygens_referral_reward"`
+	HerschelReward         float64   `db:"herschel_reward" json:"herschel_reward"`
+	HerschelReferralReward float64   `db:"herschel_referral_reward" json:"herschel_referral_reward"`
+	CassiniReward          float64   `db:"cassini_reward" json:"cassini_reward"`
+	CassiniReferralReward  float64   `db:"cassini_referral_reward" json:"cassini_referral_reward"`
+	DeviceCount            int64     `db:"device_count" json:"device_count"`
+	FromKolBonusReward     float64   `db:"from_kol_bonus_reward" json:"-"`
+	CreatedAt              time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt              time.Time `db:"updated_at" json:"-"`
+	DeletedAt              time.Time `db:"deleted_at" json:"-"`
 }
 
 type Link struct {
@@ -501,7 +502,6 @@ type UserSecret struct {
 	DeletedAt time.Time `db:"deleted_at" json:"deleted_at"`
 }
 
-
 type Subscription struct {
 	ID                      int64     `db:"id" json:"id"`
 	Company                 string    `db:"company" json:"company"`
@@ -542,7 +542,6 @@ type KOLLevelUPRecord struct {
 	ReferralNodesCount int64     `db:"referral_nodes_count" json:"referral_nodes_count"`
 	CreatedAt          time.Time `db:"created_at" json:"created_at"`
 }
-
 
 type Ads struct {
 	ID          int64     `db:"id" json:"id"`
