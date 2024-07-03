d7e7640e590d81bed40d41ab93c1886c1cf88eee
lihy <lihy@zhiannet.com>
2024-07-03 15:43:38 +0800

    fix acme ddl

--- scripts/update_20240701.sql
+++ scripts/update_20240701.sql
@@ -3,6 +3,5 @@ CREATE TABLE `acme` (
 `certificate` VARCHAR(4096) NOT NULL,
 `private_key` VARCHAR(1024) NOT NULL,
 `created_at` DATETIME(3) NOT NULL DEFAULT 0,
-`expire_at` DATETIME(3) NOT NULL DEFAULT 0,
-PRIMARY KEY (`id`)
+`expire_at` DATETIME(3) NOT NULL DEFAULT 0
 ) ENGINE = INNODB CHARSET = utf8mb4;
