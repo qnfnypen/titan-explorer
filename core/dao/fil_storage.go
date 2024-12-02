78db10617ae7dee3de02fb9892af43f9c8112e7b
qnfnypen <1256668725@qq.com>
2024-12-02 14:38:23 +0800

    fix bug of fil_storage.go

--- core/dao/fil_storage.go
+++ core/dao/fil_storage.go
@@ -23,7 +23,7 @@ func AddFilStorages(ctx context.Context, storages []*model.FilStorage) error {
 func CountFilStorage(ctx context.Context, cid string) (int64, error) {
 	var total int64
 	err := DB.GetContext(ctx, &total, fmt.Sprintf(
-		`select count(*) from %s f left join %s a on f.path = a.path where a.path <> '' and a.cid = ?`, tableNameFilStorage, tableNameAsset,
+		`select COUNT(DISTINCT(a.cid)) from %s f left join %s a on f.path = a.path where a.path <> '' and a.cid = ?`, tableNameFilStorage, tableNameAsset,
 	), cid)
 	if err != nil {
 		return 0, err
